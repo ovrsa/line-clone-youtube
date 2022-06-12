b3e20bae4b9cc2ef2888caeda1e695ac3e4e92a9
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-12 14:11:15 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseApp = firebase.initialinzeApp({
+  apiKey: "AIzaSyATiT6v699IeyaVUPV51Xy1fmIxZFFh5iU",
+  authDomain: "line-clone-99f3d.firebaseapp.com",
+  projectId: "line-clone-99f3d",
+  storageBucket: "line-clone-99f3d.appspot.com",
+  messagingSenderId: "356657676449",
+  appId: "1:356657676449:web:3f7ab20435461eebeb30e0",
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
